9524c65481b05a353b3d9360c4a7c655d4ccaba4
Alumnos <nohereily.salazar@upch.pe>
2024-09-16 16:07:13 -0500

    Actualizacion

---
+++ Actividad5/mathOperations.js
@@ -0,0 +1,7 @@
+import fetch from "node-fetch";
+const suma = (a,b) => a+b;
+const resta = (a,b) => a-b;
+const multiplicar = (a,b) => a*b;
+const dividir = (a,b) => a/b;
+
+export { suma,resta,multiplicar,dividir};
